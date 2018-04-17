c1f1298df585141bb2b660d2dc8561c722e3aaed
ZEXEND\ethanwams <infoptwo@gmail.com>
2018-04-17 16:59:51 +0300

    Initial upload to GitHub

---
+++ README.md
@@ -0,0 +1,2 @@
+Ensure you enter an API KEY in the manifest otherwise the app will not work. 
+Use https://developers.google.com/maps/documentation/javascript/get-api-key to make a key
---
+++ app/src/main/java/com/dataperspectives/bizdata/googlemapsscratch/MainActivity.java
@@ -0,0 +1,63 @@
+package com.dataperspectives.bizdata.googlemapsscratch;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Add a marker in Strathmore and move the camera
+
+        LatLng strathmore = new LatLng(-1.310082, 36.812535);
+        LatLng riara= new LatLng(-1.314735, 36.807042);
+
+        mMap.addMarker(new MarkerOptions().position(strathmore).title("Marker in Strathmore"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(strathmore));
+        mMap.addPolyline(new PolylineOptions()
+                .add(strathmore, riara)
+                .width(5)
+                .color(Color.RED));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+}
